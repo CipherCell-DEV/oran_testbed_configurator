069a99505d9001f8e6ac81269de7ca175ac390f6
Florian Frank <Florian.Frank@uni-passau.de>
2025-09-25 21:14:37 +0200

    Add install directory by default to ./repositories

--- scripts/compile/compile_gnb.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
- (
-  cd ../srsRAN_Project/ || { echo "Checkout srsRAN Project first -> Run `git submodule update --init --recursive`"; exit 1; }
-  docker build --progress=plain -t srsran-gnb .
-)
