300337a4d141799caa251fa387a3b81df166d858
Florian Frank <Florian.Frank@uni-passau.de>
2025-10-06 10:40:14 +0200

    Avoid importing override

--- scripts/src/controller/patcher/core_5g_patcher.py
+++ scripts/src/controller/patcher/core_5g_patcher.py
@@ -1,6 +1,5 @@
 import logging
 import os
-from typing import override
 
 import yaml
 
--- scripts/src/controller/patcher/near_rt_ric_patcher.py
+++ scripts/src/controller/patcher/near_rt_ric_patcher.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from typing import override, Optional
+from typing import Optional
 
 import yaml
 
