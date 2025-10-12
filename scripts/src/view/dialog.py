9b6a66c9e68139bafb8a11127bfb9373fb160a63
Florian Frank <Florian.Frank@uni-passau.de>
2025-10-12 12:00:25 +0200

    Add flag ignoring the --supress_welcome_header flag.

--- scripts/src/view/dialog.py
+++ scripts/src/view/dialog.py
@@ -142,7 +142,8 @@ def parse_command_line_arguments(argv: List[str]) -> CommandLineConfig:
         elif v.startswith("--run_demo="):
             cfg.run_demo = parse_boolean_value(v.split("=", 1)[1])
             logging.info(f"Run demo: {cfg.run_demo}")
-
+        elif v.startswith("--supress_welcome_header"):
+            pass
         else:
             logging.warning(f"Ignoring unrecognized argument: {v}")
 
