f8f407f2bb6fc1f25518b6b499f29ea35cbc8760
Alexander Braml <braml@fim.uni-passau.de>
2025-10-30 13:52:09 +0100

    Refactor traffic executor to work with multiple UEs

--- scripts/src/controller/traffic/traffic_executor.py
+++ scripts/src/controller/traffic/traffic_executor.py
@@ -1,7 +1,6 @@
 import time
-from typing import Any, Type
-
-from numpy import ndarray, dtype
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import Type
 
 from model.traffic.netcat_handler import NetcatServer, NetcatClient
 from model.traffic.traffic_config import TrafficParameters, Direction
@@ -10,38 +9,57 @@ from model.traffic.traffic_handler import TrafficServer, TrafficClient
 
 class TrafficExecutor:
 
-    def __init__(self, traffic_plan: ndarray[tuple[int], dtype[Any]]):
-        self.traffic_plan: ndarray[tuple[int], dtype[Any]] = traffic_plan
+    def __init__(self, traffic_plan: dict):
+        self.traffic_plan: dict = traffic_plan
 
     def execute(self,
                 parameters: TrafficParameters,
                 server_class: Type[TrafficServer] = NetcatServer,
                 client_class: Type[TrafficClient] = NetcatClient):
-        server_service = parameters.ue_service if parameters.direction == Direction.coreToUE else parameters.core_service
-        client_service = parameters.core_service if parameters.direction == Direction.coreToUE else parameters.ue_service
-        server_address = parameters.ue_address if parameters.direction == Direction.coreToUE else parameters.core_address
-        # TODO: Implement bidirectional traffic using two netcat listeners
+        if parameters.direction == Direction.bidirectional:
+            print('Generating bidirectional traffic is currently not supported')
+        elif parameters.direction == Direction.ueToCore:
+            print('Generating Uplink traffic is currently not supported')
+        elif parameters.direction == Direction.coreToUE:
+            clients = {}  # Always running on core, one object for each ue that traffic needs to be sent to
+            servers = {}  # UEs
+            for ue_id, conn_info in parameters.user_equipments.items():
+                clients[ue_id] = client_class(parameters.workdir, parameters.core_service, conn_info['address'],
+                                              use_nist=parameters.use_nist)
+                servers[ue_id] = server_class(parameters.workdir, conn_info['service'], conn_info['address'],
+                                              use_nist=parameters.use_nist)
 
-        server: TrafficServer = server_class(parameters.workdir, server_service, server_address)
-        client: TrafficClient = client_class(parameters.workdir, client_service, server_address)
+            for ue_id in parameters.user_equipments.keys():
+                servers[ue_id].start_session()
+                servers[ue_id].start_server()
 
-        server.start_session()
-        client.start_session()
+                clients[ue_id].start_session()
 
-        server.start_server()
+            try:
+                stop = False
+                while not stop:
+                    with ThreadPoolExecutor(max_workers=len(clients)) as executor:
+                        for values in zip(*self.traffic_plan.values()):
+                            start_time = time.time()
+                            step = dict(zip(self.traffic_plan.keys(), values))
 
-        try:
-            stop = False
-            while not stop:
-                for instant_traffic in self.traffic_plan:
-                    start_time = time.time()
-                    if instant_traffic > 0:
-                        client.send_traffic(instant_traffic, parameters.granularity - 10)
-                    rest_duration = (start_time + (parameters.granularity / 1000)) - time.time()
-                    if rest_duration > 0:
-                        time.sleep(rest_duration)
-                stop = not parameters.loop
-        finally:
-            server.stop_server()
-            server.close_session()
-            client.close_session()
+                            futures = [
+                                executor.submit(clients[key].send_traffic, val)
+                                for key, val in step.items()
+                            ]
+
+                            for future in as_completed(futures):
+                                future.result()
+
+                            rest_duration = (start_time + (parameters.granularity / 1000)) - time.time()
+                            if rest_duration > 0:
+                                time.sleep(rest_duration)
+                            if rest_duration <= 0:
+                                print(rest_duration)
+                    stop = not parameters.loop
+
+            finally:
+                for ue_id in parameters.user_equipments.keys():
+                    servers[ue_id].stop_server()
+                    servers[ue_id].close_session()
+                    clients[ue_id].close_session()
