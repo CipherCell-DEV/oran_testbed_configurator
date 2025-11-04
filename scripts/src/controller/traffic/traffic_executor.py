4593dbd9470740fac87ce235cdaf40e2f2eab3c1
Alexander Braml <braml@fim.uni-passau.de>
2025-11-04 14:02:12 +0100

    Implement uplink traffic execution

--- scripts/src/controller/traffic/traffic_executor.py
+++ scripts/src/controller/traffic/traffic_executor.py
@@ -14,39 +14,53 @@ class TrafficExecutor:
 
     def execute(self,
                 parameters: TrafficParameters,
-                server_class: Type[TrafficServer] = NetcatServer,
-                client_class: Type[TrafficClient] = NetcatClient):
+                receiver_class: Type[TrafficServer] = NetcatServer,
+                sender_class: Type[TrafficClient] = NetcatClient):
         if parameters.direction == Direction.bidirectional:
             print('Generating bidirectional traffic is currently not supported')
-        elif parameters.direction == Direction.ueToCore:
-            print('Generating Uplink traffic is currently not supported')
-        elif parameters.direction == Direction.coreToUE:
-            clients = {}  # Always running on core, one object for each ue that traffic needs to be sent to
-            servers = {}  # UEs
+        else:
+            # For UL: Clients are connections from each UE to the Core. There is only one server handling all incoming
+            # traffic from the UEs.
+            # For DL: Clients are distinct connection from the Core to each UE. There is one server running on each UE
+            # handling all incoming traffic from the Core.
+            sender = {}
+            receivers = {}
+            receiver = receiver_class(parameters.workdir, parameters.core_service, parameters.core_address,
+                                      use_nist=parameters.use_nist, nist_vm=parameters.nist_vm) \
+                if parameters.direction == Direction.ueToCore else None
             for ue_id, conn_info in parameters.user_equipments.items():
-                clients[ue_id] = client_class(parameters.workdir, parameters.core_service, conn_info['address'],
-                                              use_nist=parameters.use_nist, nist_vm=parameters.nist_vm)
-                servers[ue_id] = server_class(parameters.workdir, conn_info['service'], conn_info['address'],
-                                              use_nist=parameters.use_nist, nist_vm=parameters.nist_vm)
+                if parameters.direction == Direction.ueToCore:
+                    server_address = parameters.core_address
+                    server_service = parameters.core_service
+                    client_service = conn_info['service']
+                else:
+                    server_address = conn_info['address']
+                    server_service = conn_info['service']
+                    client_service = parameters.core_service
+
+                sender[ue_id] = sender_class(parameters.workdir, client_service, server_address,
+                                             use_nist=parameters.use_nist, nist_vm=parameters.nist_vm)
+                if parameters.direction == Direction.coreToUE:
+                    receivers[ue_id] = receiver_class(parameters.workdir, server_service, server_address,
+                                                      use_nist=parameters.use_nist, nist_vm=parameters.nist_vm)
+                else:
+                    receivers[ue_id] = receiver
 
             for ue_id in parameters.user_equipments.keys():
-                servers[ue_id].start_session()
-                servers[ue_id].start_server()
+                receivers[ue_id].start_session()
+                receivers[ue_id].start_receiver()
 
-                clients[ue_id].start_session()
+                sender[ue_id].start_session()
 
             try:
                 stop = False
                 while not stop:
-                    with ThreadPoolExecutor(max_workers=len(clients)) as executor:
+                    with ThreadPoolExecutor(max_workers=len(sender)) as executor:
                         for values in zip(*self.traffic_plan.values()):
                             start_time = time.time()
                             step = dict(zip(self.traffic_plan.keys(), values))
 
-                            futures = [
-                                executor.submit(clients[key].send_traffic, val)
-                                for key, val in step.items()
-                            ]
+                            futures = [executor.submit(sender[key].send_traffic, val) for key, val in step.items()]
 
                             for future in as_completed(futures):
                                 future.result()
@@ -60,6 +74,6 @@ class TrafficExecutor:
 
             finally:
                 for ue_id in parameters.user_equipments.keys():
-                    servers[ue_id].stop_server()
-                    servers[ue_id].close_session()
-                    clients[ue_id].close_session()
+                    receivers[ue_id].stop_receiver()
+                    receivers[ue_id].close_session()
+                    sender[ue_id].close_session()
