767f4cd8509595e7bbdc56f41c20edf947d238db
Alexander Braml <braml@fim.uni-passau.de>
2025-11-20 12:18:38 +0100

    Fix namespace generation

--- patches/templates/scripts/ue/srs_4g/ue_entrypoint.sh
+++ patches/templates/scripts/ue/srs_4g/ue_entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-if ! ip netns list | grep -qw "ue1"; then
-    sudo ip netns add ue1
+if ! ip netns list | grep -qw $1; then
+    sudo ip netns add $1
 fi
 
 exec ./build_docker/srsue/src/srsue "configs/$1_zmq.conf"
