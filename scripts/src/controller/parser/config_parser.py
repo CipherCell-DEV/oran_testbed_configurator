5830462e4a2316a8ac216dfe55b09c6179bdf917
Florian Frank <Florian.Frank@uni-passau.de>
2026-02-03 10:59:38 +0100

    Fix unmatched E   SyntaxError: f-string: unmatched '(' from github CI runner.

--- scripts/src/controller/parser/config_parser.py
+++ scripts/src/controller/parser/config_parser.py
@@ -37,7 +37,8 @@ class ConfigParser:
                 for element in implementation:
                     if element.value == params[identifier]:
                         return element
-                raise KeyError(f"Invalid {identifier.replace("_", " ")} {params[identifier]}")
+                raise KeyError(f"Invalid %s %s", identifier.replace('_', ' '),
+                               params[identifier])
             logging.warning("No %s identified", identifier.replace("_", " "))
             return None
 
